1af104fe4f54be2856ed09b1f975e5bc106e5ff5
Slappy826 <summon123@hotmail.com>
2021-01-14 22:49:09 -0600

    setup rocket example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/hello", routes![hello])
 }
