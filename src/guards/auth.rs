3fa35bf40c3295ac823f6d70fbf14e9d15438c85
Jack Fox <0xdeadbeef1@gmail.com>
2021-01-15 22:06:40 -0500

    Added dummy auth guard

--- src/guards/auth.rs
+++ src/guards/auth.rs
@@ -1,1 +1,24 @@
+use rocket::{
+    http::hyper::StatusCode,
+    request::{FromRequest, Outcome},
+    Request,
+};
 
+use super::token::TokenGuard;
+use crate::responders::error::Error as ResponseError;
+
+pub struct AuthGuard();
+
+impl AuthGuard {}
+
+#[rocket::async_trait]
+impl<'a, 'r> FromRequest<'a, 'r> for AuthGuard {
+    type Error = ResponseError;
+
+    async fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let token = try_outcome!(request.guard::<TokenGuard>().await);
+        println!("token: {:?}", token);
+
+        Outcome::Success(Self())
+    }
+}
