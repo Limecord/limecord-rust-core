9f96772c857320a63d69f8ae6c744461a0404cbc
Jack Fox <0xdeadbeef1@gmail.com>
2021-01-15 22:05:37 -0500

    Implemented auth guard for dummy user endpoint

--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -1,9 +1,9 @@
-use crate::responders::error::Error;
+use crate::{guards::auth::AuthGuard, responders::error::Error};
 
 use super::api_version_check;
 
 #[get("/<version>/users/<user>")]
-pub fn user(version: String, user: String) -> Result<(), Error> {
+pub fn user(version: String, user: String, _auth: AuthGuard) -> Result<(), Error> {
     api_version_check(version, 6)?;
 
     println!("User: {:?}", user);
