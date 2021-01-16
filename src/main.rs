cd78eea83a776bdcc8d3a5f4c7c03072641220ad
Jack Fox <0xdeadbeef1@gmail.com>
2021-01-15 22:06:08 -0500

    Catcher test implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![feature(backtrace)]
+
+use responders::error::Error;
+use rocket::{http::Status, response, Catcher, Request};
+
 #[macro_use]
 extern crate rocket;
 #[macro_use]
@@ -7,7 +12,10 @@ mod controllers;
 mod guards;
 mod responders;
 
+// fn default_catcher<'r>(req: &'r Request) -> response::Result<'r> {}
+
 #[launch]
 fn rocket() -> rocket::Rocket {
     rocket::ignite().mount("/api", controllers::get_routes())
+    // .register(catchers![Catcher::new(None, default_catcher)])
 }
